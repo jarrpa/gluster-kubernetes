d67f074a641af61aadf216e3faee3983c46e7be4
Jose A. Rivera <jarrpa@redhat.com>
2017-08-24 08:31:04 -0500

    vagrant: Improve output of rollback.sh
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- vagrant/rollback.sh
+++ vagrant/rollback.sh
@@ -6,5 +6,7 @@ vagrant sandbox rollback "${MACHINES[@]}" || exit 1
 
 for m in ${MACHINES[*]}; do
   echo "[${m}] Restarting services..."
-  vagrant ssh "${m}" -c "sudo systemctl restart docker kubelet ntpd" 1>/dev/null
+  while read -r line; do
+    echo -e "\r[${m}] ${line}"
+  done <<< "$(vagrant ssh "${m}" -c "sudo systemctl restart docker kubelet ntpd" -- -qn 2>&1 && echo "OK")" &
 done
