4bab7b6f88015f53ff2d3ee55ad51058bd21d227
Jose A. Rivera <jarrpa@redhat.com>
2018-08-27 09:13:55 -0500

    anthill-dev-local

--- vagrant/anthill.sh
+++ vagrant/anthill.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo -n "[master] Fetching anthill demo files ... "
-vagrant ssh master -c "rm -rf anthill gluster-csi-driver; git clone https://github.com/jarrpa/anthill.git --branch jarrpa-orig --single-branch; git clone https://github.com/jarrpa/gluster-csi-driver.git --branch statefulset-hack --single-branch"
+#vagrant ssh master -c "rm -rf anthill gluster-csi-driver; git clone https://github.com/jarrpa/anthill.git --branch jarrpa-orig --single-branch; git clone https://github.com/jarrpa/gluster-csi-driver.git --branch statefulset-hack --single-branch"
+vagrant ssh master -c "rm -rf anthill gluster-csi-driver" -- -q && \
+rsync -aqe "ssh -qF ssh-config" --exclude='*vendor*' --exclude='*.git*' --no-perms /home/jrivera/projects/golang/src/github.com/gluster/anthill /home/jrivera/projects/golang/src/github.com/gluster/gluster-csi-driver master: && \
 echo "OK"
 
 paplay glass.ogg
